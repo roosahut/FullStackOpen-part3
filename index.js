71607d8b6ad523a4c8b329d1cf10cdb32e417d4d
Roosa Huttunen <huttunenroosa00@gmail.com>
2022-09-15 17:20:56 +0300

    3.7-3.8 done

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 
 app.use(express.json())
+morgan.token('body', (request, response) => { return JSON.stringify(request.body) })
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
     {
